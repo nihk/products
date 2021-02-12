d32ba340fc5aa1c70d44a2d0a5de7dbfccbcd19f
Nick Rose <nihk@github.com>
2021-02-12 00:16:15 -0500

    Move jcenter() as last repository to search for

--- buildSrc/src/main/kotlin/RepositoryHandler.kt
+++ buildSrc/src/main/kotlin/RepositoryHandler.kt
@@ -2,8 +2,8 @@ import org.gradle.api.artifacts.dsl.RepositoryHandler
 
 fun RepositoryHandler.addProjectDefaults() {
     google()
-    jcenter()
     mavenCentral()
     maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
     maven { setUrl("https://jitpack.io") }
+    jcenter()
 }
