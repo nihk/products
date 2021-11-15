61cb769c2e199902edf58d43ac816160d3c49c9c
Nick Rose <nihk@github.com>
2021-11-14 21:46:04 -0500

    Add Transient ProductResult

--- products/list/src/main/kotlin/takehomeassignment/productlist/models/ProductsResult.kt
+++ products/list/src/main/kotlin/takehomeassignment/productlist/models/ProductsResult.kt
@@ -2,10 +2,13 @@ package takehomeassignment.productlist.models
 
 import takehomeassignment.localproducts.models.Product
 
-sealed class ProductsResult {
-    abstract val products: List<Product>?
-    object Started : ProductsResult() { override val products: List<Product>? = null }
-    data class Cached(override val products: List<Product>) : ProductsResult()
-    data class Fresh(override val products: List<Product>) : ProductsResult()
-    data class Error(val throwable: Throwable, override val products: List<Product>) : ProductsResult()
+sealed interface ProductsResult {
+    val products: List<Product>?
+
+    sealed class Transient : ProductsResult
+
+    object Started : Transient() { override val products: List<Product>? = null }
+    data class Cached(override val products: List<Product>) : Transient()
+    data class Fresh(override val products: List<Product>) : ProductsResult
+    data class Error(val throwable: Throwable, override val products: List<Product>) : ProductsResult
 }
