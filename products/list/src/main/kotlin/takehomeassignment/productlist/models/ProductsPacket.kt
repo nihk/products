f48c838df1d9dab0869a904e7c2a547f7b65d7dd
Nick Rose <nihk@github.com>
2021-11-21 17:49:22 -0500

    Add more ProductListViewModel tests

--- products/list/src/main/kotlin/takehomeassignment/productlist/models/ProductsPacket.kt
+++ products/list/src/main/kotlin/takehomeassignment/productlist/models/ProductsPacket.kt
@@ -7,5 +7,8 @@ sealed class ProductsPacket {
 
     data class Cached(override val products: List<Product>) : ProductsPacket()
     data class Fresh(override val products: List<Product>) : ProductsPacket()
-    data class Error(val throwable: Throwable, override val products: List<Product>) : ProductsPacket()
+    data class Error(
+        val throwable: Throwable,
+        override val products: List<Product>? = null
+    ) : ProductsPacket()
 }
--- products/list/src/test/kotlin/takehomeassignment/productlist/vm/ProductListViewModelTest.kt
+++ products/list/src/test/kotlin/takehomeassignment/productlist/vm/ProductListViewModelTest.kt
@@ -1,12 +1,13 @@
 package takehomeassignment.productlist.vm
 
 import androidx.lifecycle.SavedStateHandle
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.takeWhile
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.TestCoroutineScope
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +16,7 @@ import takehomeassignment.productlist.models.FetchProductsEvent
 import takehomeassignment.productlist.models.ProductClickedEffect
 import takehomeassignment.productlist.models.ProductClickedEvent
 import takehomeassignment.productlist.models.ProductListEffect
+import takehomeassignment.productlist.models.ProductListEvent
 import takehomeassignment.productlist.models.ProductListState
 import takehomeassignment.productlist.models.ProductsPacket
 import takehomeassignment.productlist.repository.ProductListRepository
@@ -26,49 +28,110 @@ class ProductListViewModelTest {
     val rule = CoroutinesTestRule()
 
     @Test
-    fun `should emit product states when products are fetched`() = rule.dispatcher.runBlockingTest {
-        val repository = object : ProductListRepository {
-            override fun products(): Flow<ProductsPacket> {
-                return flowOf(ProductsPacket.Fresh(products))
-            }
-        }
-        val viewModel = ProductListViewModel(repository, NoOpLogger(), SavedStateHandle(), ProductListState(), emptyList())
+    fun `should emit product states when products are fetched`() = viewModel(
+        packets = flowOf(ProductsPacket.Fresh(products))
+    ) {
+        fetchProducts()
 
-        val states = mutableListOf<ProductListState>()
-        viewModel.states
-            .onEach { states += it }
-            .takeWhile { state -> state.products == null }
-            .launchIn(this)
-
-        viewModel.processEvent(FetchProductsEvent)
-
-        val initialState = ProductListState()
-        val secondState = ProductListState().copy(isLoading = true)
-        val thirdState = ProductListState().copy(products = products)
-        assertEquals(3, states.size)
-        assertEquals(states[0], initialState)
-        assertEquals(states[1], secondState)
-        assertEquals(states[2], thirdState)
+        assertStates(
+            ProductListState(),
+            ProductListState().copy(isLoading = true),
+            ProductListState().copy(products = products),
+        )
     }
 
     @Test
-    fun `should emit product clicked effect when product is clicked`() = rule.dispatcher.runBlockingTest {
-        val repository = object : ProductListRepository {
+    fun `should handle cached when one is emitted while fetching`() = viewModel(
+        packets = flowOf(ProductsPacket.Cached(products))
+    ) {
+        fetchProducts()
+
+        assertStates(
+            ProductListState(),
+            ProductListState().copy(isLoading = true),
+            ProductListState().copy(isLoading = true, products = products),
+        )
+    }
+
+    @Test
+    fun `should handle error when one is thrown while fetching`() {
+        val throwable: Throwable = RuntimeException("Uh oh!")
+        viewModel(
+            packets = flowOf(ProductsPacket.Error(throwable = throwable))
+        ) {
+            fetchProducts()
+
+            assertStates(
+                ProductListState(),
+                ProductListState().copy(isLoading = true),
+                ProductListState().copy(error = throwable),
+            )
+        }
+    }
+
+    @Test
+    fun `should emit product clicked effect when product is clicked`() = viewModel {
+        clickProduct(id = "1234")
+
+        assertEffects(ProductClickedEffect(id = "1234"))
+    }
+
+    fun viewModel(
+        packets: Flow<ProductsPacket> = emptyFlow(),
+        initialState: ProductListState = ProductListState(),
+        initialEvents: List<ProductListEvent> = emptyList(),
+        scope: CoroutineScope = TestCoroutineScope(rule.dispatcher),
+        block: ProductListViewModelRobot.() -> Unit
+    ) {
+        ProductListViewModelRobot(packets, initialState, initialEvents, scope).block()
+    }
+
+    class ProductListViewModelRobot(
+        packets: Flow<ProductsPacket>,
+        initialState: ProductListState,
+        initialEvents: List<ProductListEvent>,
+        scope: CoroutineScope
+    ) {
+        private val repository = object : ProductListRepository {
             override fun products(): Flow<ProductsPacket> {
-                return flowOf(ProductsPacket.Fresh(products))
+                return packets
             }
         }
-        val viewModel = ProductListViewModel(repository, NoOpLogger(), SavedStateHandle(), ProductListState(), emptyList())
+        private val viewModel = ProductListViewModel(
+            repository = repository,
+            logger = NoOpLogger(),
+            handle = SavedStateHandle(),
+            initialState = initialState,
+            initialEvents = initialEvents
+        )
+        private val collectedStates = mutableListOf<ProductListState>()
+        private val collectedEffects = mutableListOf<ProductListEffect>()
 
-        val effects = mutableListOf<ProductListEffect>()
-        viewModel.effects
-            .onEach { effect -> effects += effect }
-            .takeWhile { effect -> effect !is ProductClickedEffect }
-            .launchIn(this)
+        init {
+            viewModel.states.onEach(collectedStates::add).launchIn(scope)
+            viewModel.effects.onEach(collectedEffects::add).launchIn(scope)
+        }
 
-        viewModel.processEvent(ProductClickedEvent(id = "1234"))
+        fun fetchProducts() {
+            viewModel.processEvent(FetchProductsEvent)
+        }
 
-        assertEquals(1, effects.size)
-        assertEquals(ProductClickedEffect(id = "1234"), effects.first())
+        fun clickProduct(id: String) {
+            viewModel.processEvent(ProductClickedEvent(id = id))
+        }
+
+        fun assertStates(vararg expectation: ProductListState) {
+            assertEquals(expectation.size, collectedStates.size)
+            expectation.zip(collectedStates) { expected, collected ->
+                assertEquals(expected, collected)
+            }
+        }
+
+        fun assertEffects(vararg expectation: ProductListEffect) {
+            assertEquals(expectation.size, collectedEffects.size)
+            expectation.zip(collectedEffects) { expected, collected ->
+                assertEquals(expected, collected)
+            }
+        }
     }
 }
